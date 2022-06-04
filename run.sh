01260b2a84b260f1a45a3211bca5d32e594ef03f
Alexander Dmitriev <test@example.com>
2022-06-04 21:58:10 +0300

    Fix 2

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker-compose -f "docker-compose.migrate.yml" up -d --build
-docker-compose -f "docker-compose.yml" up -d --build
+docker-compose -f "docker-compose.migrate.yml" up -d --build && docker-compose -f "docker-compose.yml" up -d --build
