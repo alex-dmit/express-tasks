ef917972f9bfd865eec80f1bf57ce4d69bb9e103
golostos <alex.golostos@gmail.com>
2022-06-05 02:42:25 +0300

    Docker no cache fix 2

--- run-migrate.sh
+++ run-migrate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose -f "docker-compose.migrate.yml" up -d --build --force-recreate
+docker-compose -f "docker-compose.migrate.yml" down
+docker-compose -f "docker-compose.migrate.yml" build --no-cache
+docker-compose -f "docker-compose.migrate.yml" up
