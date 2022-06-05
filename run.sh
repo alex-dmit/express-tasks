97e55b42ce69baa5c0f5f65c98dd8b05120815b0
golostos <alex.golostos@gmail.com>
2022-06-05 04:08:57 +0300

    Docker fix 5

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose -f "docker-compose.migrate.yml" up -d --build --force-recreate
+docker-compose -f "docker-compose.migrate.yml" up -d --build
 # docker-compose -f "docker-compose.migrate.yml" down
 # docker-compose -f "docker-compose.migrate.yml" build --no-cache
 # docker-compose -f "docker-compose.migrate.yml" up -d
