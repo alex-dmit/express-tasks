ee9f6e18a3f9997d7658c2d1eef8e09e8ff7c9c5
golostos <alex.golostos@gmail.com>
2022-06-05 04:17:09 +0300

    Fix schema

--- run-migrate.sh
+++ run-migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose -f "docker-compose.migrate.yml" up -d --build
+# docker-compose -f "docker-compose.migrate.yml" up -d --build
 
 # docker-compose -f "docker-compose.migrate.yml" down
-# docker-compose -f "docker-compose.migrate.yml" build --no-cache
-# docker-compose -f "docker-compose.migrate.yml" up -d
+docker-compose -f "docker-compose.migrate.yml" build --no-cache
+docker-compose -f "docker-compose.migrate.yml" up -d
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker-compose -f "docker-compose.migrate.yml" up -d --build
+# docker-compose -f "docker-compose.migrate.yml" up -d --build
 # docker-compose -f "docker-compose.migrate.yml" down
-# docker-compose -f "docker-compose.migrate.yml" build --no-cache
-# docker-compose -f "docker-compose.migrate.yml" up -d
+docker-compose -f "docker-compose.migrate.yml" build --no-cache
+docker-compose -f "docker-compose.migrate.yml" up -d
 
 docker-compose -f "docker-compose.yml" up -d --build
